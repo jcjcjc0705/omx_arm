56d914bd7fb948f141eb2aacf675a92fbb075af5
jcjcjc0705 <jimmy910705@gmail.com>
2026-08-12 16:55:54 +0800

    add: omx_description

---
+++ src/omx_description/launch/view_robot.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    robot_description = {
+        'robot_description': ParameterValue(
+            Command([
+                FindExecutable(name='xacro'), ' ',
+                PathJoinSubstitution([
+                    FindPackageShare('omx_description'),
+                    'urdf',
+                    'omx_f.urdf.xacro',
+                ]),
+            ]),
+            value_type=str,
+        )
+    }
+
+    rviz_config = PathJoinSubstitution(
+        [FindPackageShare('omx_description'), 'rviz', 'view_robot.rviz']
+    )
+
+    return LaunchDescription([
+        Node(package='robot_state_publisher', executable='robot_state_publisher',
+             parameters=[robot_description], output='both'),
+        Node(package='joint_state_publisher_gui', executable='joint_state_publisher_gui',
+             output='both'),
+        Node(package='rviz2', executable='rviz2', arguments=['-d', rviz_config],
+             output='log'),
+    ])
